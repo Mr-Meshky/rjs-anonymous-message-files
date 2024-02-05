f48b0387b8bac7aa2e5cdaadc883379296704e5d
MrMeshky <mr.meshky@gmail.com>
2024-02-05 17:03:06 +0330

    404 page completed

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+import { Button, Grid, Typography } from "@mui/material";
+
+import notFound from "./../assets/images/notfound.png";
+
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <Grid
+      container
+      height="calc(100vh - 190px)"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <img src={notFound} alt="404" width="40%" />
+      <Typography
+        variant="h4"
+        component="h3"
+        fontWeight="300"
+        color="red"
+        sx={{ margin: "20px" }}
+      >
+        صفحه موردنظر پیدا نشد
+      </Typography>
+      <Link to="/">
+        <Button variant="outlined" size="large">
+          بازگشت به صفحه اصلی
+        </Button>
+      </Link>
+    </Grid>
+  );
 }
 
 export default NotFoundPage;
